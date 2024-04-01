0dcd828d8230a5574168e2e74d04e43d729dff1c
mxber2022 <mxber2022@gmail.com>
2024-04-01 15:46:17 +0530

    Initial commit

---
+++ vote/app/api/[[...routes]]/route.tsx
@@ -0,0 +1,153 @@
+/** @jsxImportSource frog/jsx */
+import { Button, Frog, TextInput } from 'frog'
+import { devtools } from 'frog/dev'
+// import { neynar } from 'frog/hubs'
+import { handle } from 'frog/next'
+import { serveStatic } from 'frog/serve-static'
+import { parseEther } from 'frog'
+
+const abi = [
+  {
+    inputs: [{ internalType: 'uint256', name: 'tokenId', type: 'uint256' }],
+    name: 'mint',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+]
+
+const app = new Frog({
+  assetsPath: '/',
+  basePath: '/api',
+  // Supply a Hub to enable frame verification.
+  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+})
+ 
+app.frame('/', (c) => {
+  return c.res({
+    action: '/finish',
+    image: (
+      <div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
+        Perform a transaction
+      </div>
+    ),
+    intents: [
+      <TextInput placeholder="Value (ETH)" />,
+      <Button.Transaction target="/send-ether">Send Ether</Button.Transaction>,
+      <Button.Transaction target="/mint">Mint</Button.Transaction>,
+    ]
+  })
+})
+ 
+app.frame('/finish', (c) => {
+  const { transactionId } = c
+  return c.res({
+    image: (
+      <div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
+        Transaction ID: {transactionId}
+      </div>
+    )
+  })
+})
+ 
+app.transaction('/send-ether', (c) => {
+  const { inputText } = c
+  // Send transaction response.
+  return c.send({
+    chainId: 'eip155:10',
+    to: '0xd2135CfB216b74109775236E36d4b433F1DF507B',
+    value: parseEther(inputText),
+  })
+})
+ 
+app.transaction('/mint', (c) => {
+  const { inputText } = c
+  // Contract transaction response.
+  return c.contract({
+    abi,
+    chainId: 'eip155:10',
+    functionName: 'mint',
+    args: [69420n],
+    to: '0xd2135CfB216b74109775236E36d4b433F1DF507B',
+    value: parseEther(inputText)
+  })
+})
+
+devtools(app, { serveStatic })
+
+export const GET = handle(app)
+export const POST = handle(app)
+
+
+// /** @jsxImportSource frog/jsx */
+
+// import { Button, Frog, TextInput } from 'frog'
+// import { devtools } from 'frog/dev'
+// // import { neynar } from 'frog/hubs'
+// import { handle } from 'frog/next'
+// import { serveStatic } from 'frog/serve-static'
+
+// const app = new Frog({
+//   assetsPath: '/',
+//   basePath: '/api',
+//   // Supply a Hub to enable frame verification.
+//   // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+// })
+
+// // Uncomment to use Edge Runtime
+// // export const runtime = 'edge'
+
+// app.frame('/', (c) => {
+//   const { buttonValue, inputText, status } = c
+//   const fruit = inputText || buttonValue
+//   return c.res({
+//     image: (
+//       <div
+//         style={{
+//           alignItems: 'center',
+//           background:
+//             status === 'response'
+//               ? 'linear-gradient(to right, #432889, #17101F)'
+//               : 'black',
+//           backgroundSize: '100% 100%',
+//           display: 'flex',
+//           flexDirection: 'column',
+//           flexWrap: 'nowrap',
+//           height: '100%',
+//           justifyContent: 'center',
+//           textAlign: 'center',
+//           width: '100%',
+//         }}
+//       >
+//         <div
+//           style={{
+//             color: 'white',
+//             fontSize: 60,
+//             fontStyle: 'normal',
+//             letterSpacing: '-0.025em',
+//             lineHeight: 1.4,
+//             marginTop: 30,
+//             padding: '0 120px',
+//             whiteSpace: 'pre-wrap',
+//           }}
+//         >
+//           {status === 'response'
+//             ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
+//             : 'Welcome!'}
+//         </div>
+//       </div>
+//     ),
+//     intents: [
+//       <TextInput placeholder="Enter custom fruit..." />,
+//       <Button value="apples">Apples</Button>,
+//       <Button value="oranges">Oranges</Button>,
+//       <Button value="bananas">Bananas</Button>,
+//       status === 'response' && <Button.Reset>Reset</Button.Reset>,
+//     ],
+//   })
+// })
+
+// devtools(app, { serveStatic })
+
+// export const GET = handle(app)
+// export const POST = handle(app)
